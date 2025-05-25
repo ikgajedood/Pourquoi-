2d88fb12df7e245bafb0cb3d6954e9a6d3d44a67
ikgajedood <leyd2secs@gmail.com>
2025-05-25 13:31:56 +0200

    ajout bouton afficher tiktok

--- script2.js
+++ script2.js
@@ -7,3 +7,13 @@ function affichersnap() {
     texte.style.display = "none";
   }, 3000);
 }
+
+function affichertiktok() {
+  const texte = document.getElementById("afficherboutontiktok");
+  texte.style.display = "block";
+
+  // Cacher le texte après 3 secondes (3000 millisecondes)
+  setTimeout(() => {
+    texte.style.display = "none";
+  }, 3000);
+}
